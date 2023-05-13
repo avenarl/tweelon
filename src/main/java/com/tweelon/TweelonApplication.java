3e475fc9f1339544d8115124d38454b8028cf7ab
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-14 00:02:34 +0800

    :hatching_chick: initial commit

---
+++ src/main/java/com/tweelon/TweelonApplication.java
@@ -0,0 +1,13 @@
+package com.tweelon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweelonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweelonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tweelon/TweelonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweelon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweelonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
