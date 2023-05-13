2cf6c06bfe5811b5005c29445d6f4cac1f45842c
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-14 02:37:03 +0800

    :heavy_plus_sign: feat(repository): add tweet interface and extend jpa

---
+++ src/main/java/com/tweelon/repository/TweetRepository.java
@@ -0,0 +1,25 @@
+/**************************************************************************************
+ * TweetRepository.java 																															*
+ *																																										*
+ * Author: avenarl																																		*
+ * Created on: 05-14-23																																*
+ *																																										*
+ * Description: A TweetRepository interface for handling data access and database 		*
+ * 						  operations related to Tweet entity. Extends JpaRepository to 					*
+ * 						  leverage Spring Data JPA's built-in methods.													*
+ *																																										*
+ **************************************************************************************/
+
+package com.tweelon.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tweelon.model.Tweet;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet, Long>{
+		List<Tweet> findByUserId(Long userId);
+}
