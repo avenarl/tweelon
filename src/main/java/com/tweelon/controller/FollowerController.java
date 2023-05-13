80757494e8a6fe11f3ae6ddb80dad04329e35f63
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-14 02:45:55 +0800

    :heavy_plus_sign: feat(controller): add follower mapping

---
+++ src/main/java/com/tweelon/controller/FollowerController.java
@@ -0,0 +1,95 @@
+/**************************************************************************************
+ * FollowerController.java 																														*
+ *																																										*
+ * Author: avenarl																																		*
+ * Created on: 05-12-23																																*
+ *																																										*
+ * Description: A REST Controller for handling Follower-related operations in the 		*
+ * 						 Tweelon Application. This class maps endpoints for creating, 					*
+ * 						 deleting, and fetching tweets. 															 				  *
+ *																																										*
+ **************************************************************************************/
+
+package com.tweelon.controller;
+
+import java.util.List;
+
+import com.tweelon.model.Follower;
+import com.tweelon.service.FollowerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class FollowerController {
+
+	@Autowired
+	public FollowerService followerService;
+
+	// Create a follower
+	@PostMapping("/{userId}/{followingId}")
+	public Follower followUser(@RequestBody Long userId, @PathVariable Long followingId){
+		return followerService.followUser(userId, followingId);
+	}
+
+	// Delete a follower
+	@DeleteMapping("/{userId}/{followingId}")
+	public void unfollowUser(@PathVariable Long userId, @PathVariable Long followingId){
+		followerService.followUser(userId, followingId);
+	}
+
+	// Fetch all followers
+	@GetMapping
+	public List<Follower> getAllFollowers(){
+		return followerService.getAllFollowers();
+	}
+
+	// Fetch all followers by user id
+	@GetMapping("/user/{userId}")
+	public List<Follower> getFollowersByUserId(@PathVariable Long userId){
+		return followerService.getFollowersByUserId(userId);
+	}
+
+	// Fetch all followers by following id
+	@GetMapping("/following/{userId}")
+	public List<Follower> getFollowingByFollowingId(@PathVariable Long followingId){
+		return followerService.getFollowingByFollowingId(followingId);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
