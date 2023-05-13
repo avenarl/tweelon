a718c9b92e26ac326aa27fa05c96e3b27f61ca65
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-14 03:21:42 +0800

    :heavy_plus_sign: feat(repository): add retweet interface and extend jpa

---
+++ src/main/java/com/tweelon/repository/RetweetRepository.java
@@ -0,0 +1,34 @@
+/**************************************************************************************
+ * RetweetRepository.java 																														*
+ *																																										*
+ * Author: avenarl																																		*
+ * Created on: 05-14-23																																*
+ *																																										*
+ * Description: A RetweetRepository interface for handling data access and database 	*
+ *  						operations related to Retweet entity. Extends JpaRepository to 				*
+ *  						leverage Spring Data JPA's built-in methods.													*
+ *																																										*																															
+ **************************************************************************************/
+
+package com.tweelon.repository;
+
+import com.tweelon.model.Retweet;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface RetweetRepository extends JpaRepository<Retweet, Long>{
+
+	// SELECT * FROM retweets WHERE user_id = ?1 AND tweet_id = ?2;
+	Retweet findRetweetsByIdAndUserId(Long userId, Long tweetId);
+
+	// SELECT * FROM retweets WHERE user_id = ?1;
+  List<Retweet> findbyuserid(Long userId);
+
+	// SELECT * FROM retweets WHERE tweet_id = ?2; 
+	List<Retweet> findByTweetId(Long tweetId);
+
+}	
