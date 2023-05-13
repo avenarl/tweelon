6d85b493d660279a1b52cacaa8ff922834f0287d
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-14 00:42:28 +0800

    :up: update(main): annotate entity scan

--- src/main/java/com/tweelon/TweelonApplication.java
+++ src/main/java/com/tweelon/TweelonApplication.java
@@ -2,8 +2,10 @@ package com.tweelon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.tweelon.model")
 public class TweelonApplication {
 
 	public static void main(String[] args) {
