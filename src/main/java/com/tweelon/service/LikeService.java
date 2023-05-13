81ea57cfae57f9b637e468c1353ab27ad7ba558d
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-14 03:56:23 +0800

    :heavy_plus_sign: feat(service): add like interface with methods

---
+++ src/main/java/com/tweelon/service/LikeService.java
@@ -0,0 +1,28 @@
+/**************************************************************************************
+ * LikeService.java 																																	*
+ *																																										*
+ * Author: avenarl																																		*
+ * Created on: 05-14-23																																*
+ *																																										*
+ * Description: This interface defines the business logic for managing likes in the 	*
+ *              Tweelon Application. It provides methods for creating, retrieving, 		*
+ *              updating and deleting likes, as well as other like-related 						*
+ *              operations.																														*
+ * 																																										*
+ **************************************************************************************/
+
+package com.tweelon.service;
+
+import java.util.List;
+
+import com.tweelon.model.Like;
+
+public interface LikeService {
+
+	Like getLikeById(Like likeId, Long userId); // Retrievs a Like by its ID and the associated user ID.
+	void unLike(Long id); // Deletes a Like by its ID.
+	List<Like> getAllLikes(); // Retrieves all Likes in the system.
+	List<Like> getLikesByUserId(Long userId); // Retrieves all Likes associated with a specific user ID.
+	//
+}	
+
