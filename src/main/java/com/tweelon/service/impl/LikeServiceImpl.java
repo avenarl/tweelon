2f583dd4fcacca575efc6de61d6142fc7187472f
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-14 03:59:36 +0800

    :heavy_plus_sign: feat(service): add like service implementation

---
+++ src/main/java/com/tweelon/service/impl/LikeServiceImpl.java
@@ -0,0 +1,61 @@
+/**************************************************************************************
+ * LikeServiceImpl.java																																*
+ *																																										*
+ * Author: avenarl																																		*
+ * Created on: 05-14-23																																*
+ *																																										*
+ * Description: This class provides the implementation of the LikeService interface.	*
+ *              It handles the business logic for managing likes in the Tweelon 			*
+ *              application. It provides methods for creating, retrieving, updating, 	*
+ *              and deleting likes, as well as retrieving likes by user or tweet.			*
+ *																																										*
+ **************************************************************************************/
+
+package com.tweelon.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tweelon.model.Like;
+import com.tweelon.repository.LikeRepository;
+import com.tweelon.repository.UserRepository;
+import com.tweelon.service.LikeService;
+
+@Service
+public class LikeServiceImpl implements LikeService {
+
+	@Autowired
+	public LikeRepository likeRepository;
+
+	@Autowired
+	public UserRepository userRepository;
+
+	@Override
+	public Like getLikeById(Like likeId, Long userId){
+		// Get all likes by its ID & user ID and return
+		return likeRepository.findLikeByIdAndUserId(likeId, userId).orElseThrow(() -> new RuntimeException("Like not found."));
+	}
+
+  @Override
+	public void unLike(Long id){
+		// Remove like
+		likeRepository.deleteById(id);
+  }
+
+	@Override
+	public List<Like> getAllLikes(){
+		// Get all likes and return
+	  return likeRepository.findAll();		
+	}
+
+	@Override
+	public List<Like> getLikesByUserId(Long userId){
+		// Get all likes by user ID and return
+		List<Like> likesByUserId = likeRepository.findLikesByUserId(userId);
+		return likesByUserId;
+	}
+
+}
+
