292de7a14a48083c47c4bb58a1cd42d01dbccd50
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-22 14:41:40 +0800

    :lock: security(main): disable security auto configuration

--- src/main/java/com/tweelon/TweelonApplication.java
+++ src/main/java/com/tweelon/TweelonApplication.java
@@ -3,10 +3,11 @@ package com.tweelon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EntityScan("com.tweelon.model")
 public class TweelonApplication {
 
