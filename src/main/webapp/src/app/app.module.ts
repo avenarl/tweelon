a7a9d0dbf35eae688c8a48b08b35de42dc6dd05c
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-30 21:04:25 +0800

    :star: feat(component): generate register

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UserService } from './services/user.service';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegisterComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [UserService],
   bootstrap: [AppComponent],
