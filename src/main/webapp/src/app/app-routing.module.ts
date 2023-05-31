7fddecbd89e8d023a3ad551b06bbb20a7af42caf
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-31 17:36:23 +0800

    :heavy_plus_sign: feat(routing): add routing for register component

---
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: '', component: RegisterComponent },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
