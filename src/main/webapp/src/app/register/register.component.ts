10c23cc92f0fb3a7300491bcc473e8125e565eda
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-01 00:35:17 +0800

    :heavy_plus_sign: feat(component): add register method

--- src/main/webapp/src/app/register/register.component.ts
+++ src/main/webapp/src/app/register/register.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  user: any = {};
 
+  constructor(private userService: UserService) {}
+
+  register() {
+    this.userService.register(this.user).subscribe(
+      (user) => console.log('User registered successfully!'),
+      (error) => console.log('Error registering user', error)
+    );
+  }
 }
