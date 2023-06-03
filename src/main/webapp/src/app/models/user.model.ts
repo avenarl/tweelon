49385a8c25372b230ae54e6a17eaedc2847b4098
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-03 18:40:34 +0800

    :heavy_plus_sign: feat(model): add user interface

---
+++ src/main/webapp/src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+  username: string;
+  password: string;
+  email: string;
+  displayName: string;
+  bio: string;
+  profileImage: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
