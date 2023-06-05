b1c6b5324c2384626487ed2c1c3f57d451dea932
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-05 10:01:37 +0800

    :heavy_plus_sign: feat(component): generate login

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { UserService } from './services/user.service';
 import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent],
+  declarations: [AppComponent, RegisterComponent, LoginComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
