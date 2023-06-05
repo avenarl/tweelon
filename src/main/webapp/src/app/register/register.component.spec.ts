b5969fc7e0edd9f00f13fcf026fb877ba1029604
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-05 11:03:23 +0800

    :heavy_plus_sign: test(component): call register method

--- src/main/webapp/src/app/register/register.component.spec.ts
+++ src/main/webapp/src/app/register/register.component.spec.ts
@@ -1,23 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { of } from 'rxjs';
+import { User } from '../models/user.model';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let userService: UserService;
+  let userServiceSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterComponent],
+      imports: [HttpClientTestingModule, FormsModule],
+      providers: [UserService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    userService = TestBed.get(UserService);
+
+    const mockUser: User = {
+      username: 'testUser',
+      email: 'testuser@example.com',
+      password: 'testpassword',
+      displayName: 'Test User',
+      bio: 'This is a test bio',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    userServiceSpy = spyOn(userService, 'register').and.returnValue(
+      of(mockUser)
+    );
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Register
+  it('should call register on the service when register is called', () => {
+    const user: User = {
+      username: 'testUser',
+      email: 'testuser@example.com',
+      password: 'testpassword',
+      displayName: 'Test User',
+      bio: 'This is a test bio',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    component.user = user;
+    component.register();
+    expect(userServiceSpy).toHaveBeenCalled();
   });
 });
