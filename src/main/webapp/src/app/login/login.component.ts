d7aa8e1ef9a1b4983d45371dc638f5e53d428dad
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-06 19:08:24 +0800

    :heavy_plus_sign: feat(component): add login method

--- src/main/webapp/src/app/login/login.component.ts
+++ src/main/webapp/src/app/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  user: any = {};
 
+  constructor(private userService: UserService) {}
+
+  login() {
+    this.userService.login(this.user).subscribe(
+      (user) => console.log('User logged in successfully!'),
+      (error) => console.log('Error logging in', error)
+    );
+  }
 }
