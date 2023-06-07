c71dd022ae62683cad48ff9f8e874093bb872748
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-07 22:07:52 +0800

    :shirt: refactor(controller): initialize password encoder for login

--- src/main/java/com/tweelon/controller/UserController.java
+++ src/main/java/com/tweelon/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,20 +31,24 @@ public class UserController {
 	@Autowired
 	public UserRepository userRepository;
 
+	@Autowired
+	public PasswordEncoder passwordEncoder;
+
 	// Login
 	@PostMapping("/login")
 	public ResponseEntity<User> loginUser(@RequestBody User user){
 
 		Optional<User> usernameEntry = userRepository.findByUsername(user.getUsername());
 
-		if(usernameEntry.isPresent()){
+		if(!usernameEntry.isPresent()){
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect Username");
 		}
 
 		User existingUser = usernameEntry.get();
-		if(!existingUser.getPassword().equals(user.getPassword())) {
+
+ 		if(!passwordEncoder.matches(user.getPassword(), existingUser.getPassword())) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect Password");
-		}
+    }
 
 		return new ResponseEntity<>(existingUser, HttpStatus.OK);
 
