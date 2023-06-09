1bed007ea96592d34eaabf8aa729dffc8f734e9e
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-09 09:37:04 +0800

    :heavy_plus_sign: feat(service): generate tweet

---
+++ src/main/webapp/src/app/services/tweet.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tweet } from './tweet.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TweetService {
+  private baseUrl = 'http://localhost:8080/api/v1/tweet';
+
+  constructor(private http: HttpClient) {}
+
+  getTweets(): Observable<Tweet[]> {
+    return this.http.get<Tweet[]>(`${this.baseUrl}/tweets`);
+  }
+}
