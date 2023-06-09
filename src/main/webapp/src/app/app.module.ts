ee547ea11b44c7cd0ec540c0419906ea2a972ab5
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-09 10:30:12 +0800

    :up: feat(module): import tweet service

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UserService } from './services/user.service';
+import { TweetService } from './services/tweet.service';
 import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { TweetComponent } from './tweet/tweet.component';
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    TweetComponent,
+  ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
-  providers: [UserService],
+  providers: [UserService, TweetService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
