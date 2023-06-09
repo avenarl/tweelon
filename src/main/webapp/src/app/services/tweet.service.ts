7766d36fb3127a77276c0cbe1915294ab3d28d0b
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-09 10:40:15 +0800

    :up: feat(service): add tweet url

--- src/main/webapp/src/app/services/tweet.service.ts
+++ src/main/webapp/src/app/services/tweet.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Tweet } from './tweet.model';
+import { Tweet } from '../models/tweet.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TweetService {
-  private baseUrl = 'http://localhost:8080/api/v1/tweet';
+  private apiServerUrl = 'http://localhost:8080/api/v1/tweet';
 
   constructor(private http: HttpClient) {}
 
   getTweets(): Observable<Tweet[]> {
-    return this.http.get<Tweet[]>(`${this.baseUrl}/tweets`);
+    return this.http.get<Tweet[]>(`${this.apiServerUrl}/tweets`);
   }
 
   createTweet(userId: number, tweet: Tweet): Observable<Object> {
-    return this.http.post(`${this.baseUrl}/${userId}`, tweet);
+    return this.http.post(`${this.apiServerUrl}/${userId}`, tweet);
   }
 }
