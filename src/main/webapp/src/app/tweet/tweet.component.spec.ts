d13fb6e00d6ff2e7958c5b5ef6198be765dce59d
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-11 16:06:54 +0800

    :heavy_plus_sign: test(component): add test case tweet

--- src/main/webapp/src/app/tweet/tweet.component.spec.ts
+++ src/main/webapp/src/app/tweet/tweet.component.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 import { TweetComponent } from './tweet.component';
+import { TweetService } from '../services/tweet.service';
+import { Tweet } from '../models/tweet.model';
 
 describe('TweetComponent', () => {
   let component: TweetComponent;
   let fixture: ComponentFixture<TweetComponent>;
+  let tweetService: TweetService;
+
+  const tweetServiceStub = {
+    getTweets: () => of([{} as Tweet]),
+    createTweet: () => of({}),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TweetComponent ]
-    })
-    .compileComponents();
+      declarations: [TweetComponent],
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: TweetService, useValue: tweetServiceStub }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TweetComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    tweetService = TestBed.inject(TweetService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getTweets on init', () => {
+    const spy = spyOn(tweetService, 'getTweets').and.callThrough();
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should call createTweet when creating a tweet', () => {
+    const spy = spyOn(tweetService, 'createTweet').and.callThrough();
+    component.createTweet();
+    expect(spy).toHaveBeenCalled();
   });
 });
