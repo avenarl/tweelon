538aa9e4b7293a29da3f12273744f4f54ecaeaa5
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-11 16:50:05 +0800

    :up: update(routes): set tweet path

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { TweetComponent } from './tweet/tweet.component';
 
 const routes: Routes = [
   { path: '', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'tweet', component: TweetComponent },
   { path: '**', redirectTo: '' },
 ];
 
